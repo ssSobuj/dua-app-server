9903f11f89084120c6b3f5442008e2da1444adff
ssSobuj <react.tork@gmail.com>
2024-11-21 19:24:03 +0600

    fix route

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const apiRoutes = require("./routes/api");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+// app.use("/api", apiRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`); // Use backticks here
+// });
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`); // Use backticks here
-});
+module.exports = app;
